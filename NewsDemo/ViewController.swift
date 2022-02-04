71b22059076daaa766bfa3065e383a37f41fdfd6
Mostafa abdelhafez <mostafaaladar@gmail.com>
2022-02-04 23:13:25 +0200

    Initial Commit

---
+++ NewsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsDemo
+//
+//  Created by Mostafa on 2/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
